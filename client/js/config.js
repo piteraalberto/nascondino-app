c1c8e0acccadc9e4bb59f071dec248089625248a
piteraalberto <pitera.alberto@gmail.com>
2026-07-12 03:47:04 +0200

    Aggiornato il file config.js

--- client/js/config.js
+++ client/js/config.js
@@ -6,5 +6,5 @@
 const CONFIG = {
   SERVER_URL: window.location.hostname === 'localhost'
     ? 'http://localhost:3001'
-    : 'https://TUO-BACKEND.onrender.com', // <-- SOSTITUISCI dopo il deploy su Render
+    : 'https://nascondino-server.onrender.com',
 };
